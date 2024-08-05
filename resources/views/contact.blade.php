095a6f32e87e454a4c8b0fadb50b357d2549fb37
AGUN1505 <agun1505@gmail.com>
2024-08-05 12:37:38 +0700

    penambahan tailwind

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>kontak</title>
 </head>
 
 <body>
+    <a href="/">home</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+    <a href="/about">about</a>
     <p>emailku@gmail.com</p>
     <p>jadi saya siapa</p>
 </body>
