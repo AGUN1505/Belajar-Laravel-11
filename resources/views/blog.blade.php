94879903851e3daf944b0622d998c7f216478873
AGUN1505 <agun1505@gmail.com>
2024-08-05 11:36:21 +0700

    menambah view, route, css dan js

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>lorem</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum repellendus placeat minima quisquam molestiae
+        sequi illum officia quia veritatis maiores sed doloremque, architecto magni incidunt iste ratione sit doloribus
+        facilis.</p>
+    <h1>aduh</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, magnam soluta commodi molestiae, nihil culpa
+        ducimus ad aliquid error illum blanditiis sit perferendis deserunt vitae quo! Facere temporibus nihil porro.</p>
+</body>
+
+</html>
